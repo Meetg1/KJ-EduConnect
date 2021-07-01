d6ea57fb136b6babd589f03f6c2a70ce89e928ef
shubh0208 <62873923+shubh0208@users.noreply.github.com>
2021-07-01 15:53:53 +0530

    Added preview docs

--- app.js
+++ app.js
@@ -1021,6 +1021,8 @@ app.get("/single_material/:document_id", async function (req, res) {
     })
     .populate("author");
 
+  
+    console.log("abcdd "+doc.driveId);
   if (!doc) {
     req.flash("danger", "Cannot find that document!");
     return res.redirect("back");
